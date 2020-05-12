38915c0bb74898f61daf81e24c5dc5f0671473d5
dy956122 <dy956122@gmail.com>
2020-05-12 16:41:03 +0800

    第三次作業

---
+++ Unity_IfSwitchLoop/Assets/Scripts/SliderController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderController : MonoBehaviour
+{
+    private int Hp;
+    public int Hp1 { get => Hp; set => Hp = value; }
+
+
+    public Text hint;
+    public Text drinkSomething;
+    public string propDrink;
+
+    void Update()
+    {
+        if (Hp <= 30)
+        {
+            hint.text = "危險!!";
+        }
+        else if (Hp <= 70)
+        {
+            hint.text = " 警告!";
+        }
+        else
+        {
+            hint.text = "安全~";
+        }
+
+        switch (propDrink)
+        {
+            case "紅水":
+                drinkSomething.text = "回復血量" ;
+                break;
+
+            default:
+                break;
+        }
+
+
+    }
+}
