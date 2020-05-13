2ad45e8d416e6e090491fce88601b6e6e1beacc0
dy956122 <dy956122@gmail.com>
2020-05-14 00:59:46 +0800

    追加Cube陣列

--- Unity_IfSwitchLoop/Assets/Scripts/SliderController.cs
+++ Unity_IfSwitchLoop/Assets/Scripts/SliderController.cs
@@ -5,16 +5,26 @@ using UnityEngine.UI;
 
 public class SliderController : MonoBehaviour
 {
-    private int Hp;
-    public int Hp1 { get => Hp; set => Hp = value; }
-
+    private int Hp = 100;
+    public string propDrink;
 
     public Text hint;
     public Text drinkSomething;
-    public string propDrink;
+  
+    public Slider HpSlider;
+
+    public GameObject Cube;
+    //public Slider HpSlider1 { get => HpSlider; set => HpSlider = value; }
+
+    private void Awake()
+    {
+        CubeArray(10);
+    }
+
 
     void Update()
     {
+        #region 血量 If 判斷式
         if (Hp <= 30)
         {
             hint.text = "危險!!";
@@ -27,17 +37,24 @@ public class SliderController : MonoBehaviour
         {
             hint.text = "安全~";
         }
+        #endregion 血量 If 判斷式 結束
 
-        switch (propDrink)
-        {
-            case "紅水":
-                drinkSomething.text = "回復血量" ;
-                break;
+        drinkSomething.text = propDrink == "紅水" ? "恢復血量" : propDrink == "藍水" ? "恢復魔力" : "你吃錯藥了!!";
 
-            default:
-                break;
-        }
-
-
+        
     }
+
+    #region 設定方塊陣列
+    public void CubeArray(int length)
+    {
+        for (int i = 0; i <= length; i++)
+        {
+            for (int k = 0; k < i; k++)
+            {
+                Instantiate(Cube, new Vector3(i, k, 0), Quaternion.Euler(270, 0, 0));
+            }
+        }
+    }
+    #endregion 設定方塊陣列 結束
+
 }
